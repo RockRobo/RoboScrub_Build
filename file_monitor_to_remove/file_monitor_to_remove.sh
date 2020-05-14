5c4713138de42e65ac9e23ccff026a8660eec13e
wangjie <wangjie@roborock.com>
2020-05-14 17:44:51 +0800

    add sh rm avi

---
+++ file_monitor_to_remove/file_monitor_to_remove.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#!/usr/bin/env
+exit #
+source /etc/profile
+source /etc/profile
+
+
+file_suffix="*.avi"
+absolute_path="/ssd2/"
+all_result=`ls $absolute_path$file_suffix -alrc | awk {'print$9'}` #
+#maxsize=$((1024*10)) 
+maxsize=90 #M
+GOING="ongoing"
+
+for file in $all_result
+do
+filesize=`du -sm  $absolute_path |awk '{print $1}'` #
+if [ $filesize -gt $maxsize ]
+then
+	result=$(echo $file | grep "${GOING}")
+	if [[ "$result" = "" ]]
+	then
+	    	echo "$file not include $GOING***$result"
+		rm -rf $file
+	else
+	    	echo "$file include $GOING***$result"
+	fi
+else
+	echo "***maxsize == $maxsize***filesize == $filesize***"
+fi
+done
+
+
+
+
+
+
+
+#echo "wj1" >> /var/log/test.log
+
+
+
