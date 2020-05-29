62ab46963333b46d82686fd20748e006ac5317eb
wangjie <wangjie@roborock.com>
2020-05-29 13:58:05 +0800

    add disk usage check

--- file_monitor_to_remove/file_monitor_to_remove.sh
+++ file_monitor_to_remove/file_monitor_to_remove.sh
@@ -6,27 +6,33 @@ source /etc/profile
 
 
 file_suffix="*.avi"
-absolute_path="/ssd2/"
-all_result=`ls $absolute_path$file_suffix -alrc | awk {'print$9'}` 
+#absolute_path="/ssd2/"
+absolute_path="/home/roborock/scrubber_videos/"
+all_result=`ls $absolute_path$file_suffix -alrt | awk {'print$9'}` #
 #maxsize=$((1024*10)) 
-maxsize=90 #M
+maxsize=300 #M
 GOING="ongoing"
+max_disk_usage=90
+disk_path="/dev/mmcblk0p1"
+current_disk_usage=`df -P ${2} |  grep $disk_path | awk '{print $5 }' | cut -d'%' -f1 | tr -d '\n'`
+#echo "current_disk_usage == $current_disk_usage "
+#echo "max_disk_usage == $max_disk_usage "
 
 for file in $all_result
 do
-filesize=`du -sm  $absolute_path |awk '{print $1}'` 
+filesize=`du -sm  $absolute_path |awk '{print $1}'` #
 if [ $filesize -gt $maxsize ]
 then
 	result=$(echo $file | grep "${GOING}")
-	if [[ "$result" = "" ]]
+	if [[ "$result" = "" ]] || [[ $current_disk_usage -gt $max_disk_usage ]]
 	then
-	    	echo "$file not include $GOING***$result"
+	    	echo "rm -- $file not include $GOING***$result or gt max_disk_usage"
 		rm -rf $file
 	else
 	    	echo "$file include $GOING***$result"
-	fi
+	fi	
 else
-	echo "$maxsize***filesize == $filesize"
+	echo "$maxsize***$filesize"
 fi
 done
 
@@ -36,7 +42,18 @@ done
 
 
 
+
+
+
+
+
+
+
+
+
+
 #echo "wj1" >> /var/log/test.log
+
 
 
 
