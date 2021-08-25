f57df2a0f214c3cc36bf831489d6fd4bbed10e20
guomingli <1260306690@qq.com>
2021-08-25 11:04:56 +0800

    Add files via upload

--- auto_test_bash/mission_start.bash
+++ auto_test_bash/mission_start.bash
@@ -2,8 +2,9 @@
 sensor_name=sensor_monitor.yaml
 base_name=roborock_base-start
 echo "mission start"
-rm -rf /home/roborock/auto_test/data/rosbag
-rm -rf /home/roborock/auto_test/data/log
+
+echo "roborock"|sudo -S rm -rf /home/roborock/auto_test/data/rosbag
+echo "roborock"|sudo -S rm -rf /home/roborock/auto_test/data/log
 sleep 60s
 
 gnome-terminal -e "roslaunch scrubber_gazebo scrubber_testworld.launch"
@@ -12,7 +13,11 @@ sleep 20s
 
 rosnode kill /amcl
 echo "kill amcl"
-sleep 20s
+rosnode kill /app_processor
+echo "kill app_processor"
+rosnode kill /state_machine
+echo "kill state_machine"
+sleep 60s
 
 /usr/bin/expect <<EOF
 set timeout 300000
@@ -29,7 +34,7 @@ expect "*#"
 send "rm -rf *\r"
 expect "*#"
 send "cd /mnt/disk1\r"
-send "python main.py 7 6 1\r"
+send "python main.py 7 7 1\r"
 expect "*#"
 send "scp text.txt roborock@192.168.1.20:/home/roborock/auto_test\r"
 expect "*password:"
